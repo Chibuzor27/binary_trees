82559b905bc894780b1b014c753abe35a1c5739e
Chibuzor <chibuzornwankwo@ymail.com>
2024-01-03 01:27:07 -0800

    initial commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/*
+ * binary_tree_node - function
+ * @parent: parent node
+ * @value: node value
+ *
+ * Return: binary tree
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return parent;
+
+	node->n = value;
+
+	if (parent == NULL)
+		return node;
+
+	if (value <= parent->n)
+		parent->left = node;
+	else
+		parent->right = node;
+
+	node->parent = parent;
+	return node;
+}
