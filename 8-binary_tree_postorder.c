3dbc17dc2d3756145ed6c1dbe46d738ee03638c0
Chibuzor <chibuzornwankwo@ymail.com>
2024-01-03 05:56:21 -0800

    update

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,12 +9,12 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL || func != NULL)
+	if (tree != NULL && func != NULL)
 	{
 		if (tree->left != NULL)
 			binary_tree_postorder(tree->left, func);
 		if (tree->right != NULL)
 			binary_tree_postorder(tree->right, func);
-		printf("%d\n", tree->n);
+		func(tree->n);
 	}
 }
