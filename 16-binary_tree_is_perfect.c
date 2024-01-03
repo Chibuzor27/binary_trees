744ad69c6739c315d485f2e6cead95eaf09842e2
Chibuzor <chibuzornwankwo@ymail.com>
2024-01-03 04:47:04 -0800

    update

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+int depth(binary_tree_t *tree);
+int max(int num1, int num2);
+
+/**
+ * binary_tree_is_perfect - function
+ * @tree: arg
+ * Return: size
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if ((tree->left != NULL && tree->right == NULL) ||
+		(tree->left == NULL && tree->right != NULL))
+		return (0);
+
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right) &&
+			depth(tree->left) == depth(tree->right));
+	}
+}
+
+/**
+ * depth - function
+ * @tree: arg
+ * Return: int
+ */
+int depth(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (max(depth(tree->left), depth(tree, right)));
+}
+
+/**
+ * max - function
+ * @num1: arg
+ * @num2: arg
+ * Return: int
+ */
+int max(int num1, int num2)
+{
+	if (num1 > num2)
+		return (num1);
+	else
+		return (num2);
+}
